e2ff0647562c765dddf90948bf2b08b50c827fb5
Filippo Bisconcin <filippo.bisconcin@gmail.com>
2017-02-07 22:06:32 +0100

    change download method

--- application/helpers/opendata_helper.php
+++ application/helpers/opendata_helper.php
@@ -4,17 +4,13 @@ abstract class OpenData {
 
     protected $url = null;
     protected $content = null;
-    private $_id = null;
+    protected $_id = null;
     private $CI;
 
     public function __construct($url, $id) {
         $this->url = $url;
         $this->_id = $id;
         $this->CI = &get_instance();
-    }
-
-    private static function getTableName($id) {
-        return "opendata_$id";
     }
 
     function delete_row(&$array, $offset) {
@@ -42,38 +38,29 @@ abstract class OpenData {
 
     //db
 
-    public function _db_table_exist() {
-        return $this->CI->db->table_exists(self::getTableName($this->_id));
-    }
-
     public function _db_insert_data($data) {
-        if ($this->_db_table_exist()) {
-            $this->_db_clear_data();
-        } else {
-            $this->_db_create_table();
+
+        if (!is_array($data)) {
+            throw new Exception("deve essere array");
         }
-        $this->CI->db->insert_batch(self::getTableName($this->_id), $data);
+
+        if (!(array_key_exists("pm10", $data) && array_key_exists("azoto", $data) && array_key_exists("ozono", $data) )) {
+            throw new Exception("deve contenere pm10,azoto e azono");
+        }
+
+        $this->_db_ignore_batch_insert("dati_pm10", $data["pm10"]);
+        $this->_db_ignore_batch_insert("dati_ozono", $data["ozono"]);
+        $this->_db_ignore_batch_insert("dati_azoto", $data["azoto"]);
     }
 
-    public function _db_clear_data() {
-        $this->CI->db->where("1", "1");
-        $this->CI->db->delete(self::getTableName($this->_id));
-    }
-
-    public function _db_create_table() {
-        $_tn = self::getTableName($this->_id);
-        //SQL DDL
-        $this->CI->db->query("CREATE TABLE IF NOT EXISTS $_tn ("
-                . 'id int(11) NOT NULL,'
-                . 'lat decimal(12,8) NOT NULL,'
-                . 'lng decimal(12,8) NOT NULL,'
-                . 'ozono varchar(20) DEFAULT NULL,'
-                . 'pm10 varchar(20) DEFAULT NULL,'
-                . 'data_ozono varchar(20) DEFAULT NULL,'
-                . 'data_pm10 varchar(20) DEFAULT NULL,'
-                . 'weight decimal(10,5) NOT NULL DEFAULT \'1.00000\','
-                . 'PRIMARY KEY (id)'
-                . ') ENGINE=InnoDB DEFAULT CHARSET=utf8;');
+    private function _db_ignore_batch_insert($tb, $fields) {
+        $this->CI->db->trans_start();
+        foreach ($fields as $item) {
+            $insert_query = $this->CI->db->insert_string($tb, $item);
+            $insert_query = str_replace('INSERT INTO', 'INSERT IGNORE INTO', $insert_query);
+            $this->CI->db->query($insert_query);
+        }
+        $this->CI->db->trans_complete();
     }
 
     //abstract
--- application/libraries/opendata/Aria.php
+++ application/libraries/opendata/Aria.php
@@ -6,78 +6,65 @@ class Aria extends JSONOpenData {
         parent::__construct("http://89.96.234.233/aria-json/exported/aria/coords.json", 1);
     }
 
-    private function _dati() {
-        $stazioni = file_get_contents("http://89.96.234.233/aria-json/exported/aria/data.json");
-        $stazioni = json_decode($stazioni);
-
-        $array = $stazioni->stazioni;
-        foreach ($array as $key => $value) {
-            if (count($value->misurazioni) == 0) {
-                unset($array[$key]);
-            } else {
-                foreach ($value->misurazioni as $key2 => $value2) {
-                    if (isset($value2->pm10)) {
-                        if (count($value2->pm10) > 1) {
-                            $array[$key]->misurazioni[$key2]->pm10 = end($value2->pm10);
-                        }
-                    }
-                    if (isset($value2->ozono)) {
-                        if (count($value2->ozono) > 1) {
-                            $array[$key]->misurazioni[$key2]->ozono = end($value2->ozono);
-                        }
-                    }
-                }
-            }
-        }
-        return $array;
+    public function _formato_to_timestamp($input) {
+        //2017-02-05 19:10:01
+        $date = DateTime::createFromFormat('Y-m-d H:i:s', $input);
+        return $input; //$date->format('U');
     }
 
     public function _stazioni($file) {
-        $out = array();
+        $stazioni = array();
         foreach ($file->coordinate as $row) {
             //$row = $row[0];
-            $out[$row->codseqst] = $row;
-            unset($out[$row->codseqst]->codseqst);
+            $stazioni[$row->codseqst] = $row;
+            unset($stazioni[$row->codseqst]->codseqst);
         }
-        return $out;
+        return $stazioni;
     }
 
     public function custom_parse() {
         $stazioni = $this->_stazioni($this->content);
-        $dati = $this->_dati();
 
-        $out = array();
-        foreach ($dati as $misurazione) {
+        $mis_staz = file_get_contents("http://89.96.234.233/aria-json/exported/aria/data.json");
+        $mis_staz = json_decode($mis_staz);
 
-            $ozono = null;
-            $data_ozono = null;
-            $pm10 = null;
-            $data_pm10 = null;
+        $_pm10 = array();
+        $_ozono = array();
 
-            if (is_array($misurazione->misurazioni)) {
-                foreach ($misurazione->misurazioni as $m) {
-                    if (isset($m->ozono)) {
-                        $ozono = $m->ozono->mis;
-                        $data_ozono = $m->ozono->data;
-                    } elseif (isset($m->pm10)) {
-                        $pm10 = $m->pm10->mis;
-                        $data_pm10 = $m->pm10->data;
+        foreach ($mis_staz->stazioni as $key => $value) {
+            if (count($value->misurazioni) == 0) {
+                unset($mis_staz->stazioni[$key]);
+            } else {
+                foreach ($value->misurazioni as $key2 => $value2) {
+
+                    $common = array(
+                        "id_stazione" => $value->codseqst,
+                        "lat" => $stazioni[$value->codseqst]->lat,
+                        "lng" => $stazioni[$value->codseqst]->lon,
+                        "id_opendata" => $this->_id); //da db
+
+
+                    if (isset($value2->pm10) && count($value2->pm10)) {
+
+                        foreach ($value2->pm10 as $mis) {
+                            $k = $_pm10[] = array_merge($common, array("data" => $this->_formato_to_timestamp($mis->data), "valore" => $mis->mis));
+                        }
+                    }
+                    if (isset($value2->ozono) && count($value2->ozono)) {
+
+                        foreach ($value2->ozono as $mis) {
+                            $_ozono[] = array_merge($common, array("data" => $this->_formato_to_timestamp($mis->data), "valore" => $mis->mis));
+                        }
                     }
                 }
             }
-
-            if (!(is_null($ozono) && is_null($pm10))) {
-                $out[] = array(
-                    "id" => $misurazione->codseqst,
-                    "lat" => $stazioni[$misurazione->codseqst]->lat,
-                    "lng" => $stazioni[$misurazione->codseqst]->lon,
-                    "ozono" => $ozono,
-                    "data_ozono" => $data_ozono,
-                    "pm10" => $pm10,
-                    "data_pm10" => $data_pm10
-                );
-            }
         }
+
+        unset($mis_staz);
+
+        $out = array("pm10" => $_pm10, "ozono" => $_ozono, "azoto" => array());
+
+        //echo "<pre>" . print_r($out, true) . "</pre>";
 
         return $out;
     }
